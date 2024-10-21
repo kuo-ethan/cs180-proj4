1c94a04d2e2ab0b13836509f48efd93724288892
Ethan Kuo <ek0335@berkeley.edu>
2024-10-21 13:19:07 -0700

    done with basic mosaic

--- code/main.py
+++ code/main.py
@@ -1,15 +1,131 @@
 import skimage.io as skio
-from utils import rectify_image
+from utils import rectify_image, build_mosaic, display_img_with_keypoints
 
-# Image rectification
-ART_KEYPOINTS = [[87, 98], [138, 128], [259, 127], [197, 97]]
-ART_RECTANGLE = [[0, 0], [200, 0], [200, 200], [0, 200]]
-im = skio.imread(f'../data/art.jpg')
-rectified_im = rectify_image(im, ART_KEYPOINTS, ART_RECTANGLE)
-skio.imsave(f'../images/rectified_art.jpg', rectified_im)
 
-IPAD_KEYPOINTS = [(40, 188), (172, 503), (462, 363), (264, 135)]
-IPAD_RECTANGLE = [(0, 0), (281 * 2, 0), (281 * 2, 215 * 2), (0, 215 * 2)]
-im = skio.imread(f'../data/ipad.jpg')
-rectified_im = rectify_image(im, IPAD_KEYPOINTS, IPAD_RECTANGLE)
-skio.imsave(f'../images/rectified_ipad.jpg', rectified_im)
+# Drivers
+def image_rectification():
+    ART_KEYPOINTS = [[87, 98], [138, 128], [259, 127], [197, 97]]
+    ART_RECTANGLE = [[0, 0], [0, 200], [200, 200], [200, 0]]
+    im = skio.imread(f"../data/art.jpg")
+    rectified_im = rectify_image(im, ART_KEYPOINTS, ART_RECTANGLE)
+    skio.imsave(f"../images/rectified_art.jpg", rectified_im)
+
+    IPAD_KEYPOINTS = [(40, 188), (172, 503), (462, 363), (264, 135)]
+    IPAD_RECTANGLE = [(0, 0), (0, 281 * 2), (215 * 2, 281 * 2), (215 * 2, 0)]
+    im = skio.imread(f"../data/ipad.jpg")
+    rectified_im = rectify_image(im, IPAD_KEYPOINTS, IPAD_RECTANGLE)
+    skio.imsave(f"../images/rectified_ipad.jpg", rectified_im)
+
+
+def mosaic_blending():
+    im1 = skio.imread(f"../data/balcony1.JPG")
+    im2 = skio.imread(f"../data/balcony2.JPG")
+    BALCONY_KEYPOINTS1 = [
+        (443, 465),
+        (477, 467),
+        (478, 560),
+        (460, 437),
+        (355, 376),
+        (472, 378),
+        (497, 416),
+        (700, 425),
+        (877, 572),
+        (798, 250),
+        (739, 278),
+        (967, 241),
+    ]
+
+    BALCONY_KEYPOINTS2 = [
+        (204, 479),
+        (254, 478),
+        (253, 587),
+        (233, 442),
+        (56, 367),
+        (250, 373),
+        (282, 417),
+        (490, 420),
+        (608, 527),
+        (563, 275),
+        (522, 292),
+        (660, 284),
+    ]
+
+    # display_img_with_keypoints(im1, BALCONY_KEYPOINTS1)
+    # display_img_with_keypoints(im2, BALCONY_KEYPOINTS2)
+    mosaic = build_mosaic(im1, im2, BALCONY_KEYPOINTS1, BALCONY_KEYPOINTS2)
+    skio.imsave(f"../images/balcony_mosaic.jpg", mosaic)
+
+    im1 = skio.imread(f"../data/livingroom1.JPG")
+    im2 = skio.imread(f"../data/livingroom2.JPG")
+
+    LIVINGROOM_KEYPOINTS1 = [
+        (298, 355),
+        (268, 210),
+        (255, 142),
+        (438, 174),
+        (442, 244),
+        (447, 321),
+        (699, 216),
+        (688, 288),
+        (169, 343),
+        (222, 376),
+        (243, 263),
+        (295, 109),
+        (665, 158),
+        (35, 44),
+    ]
+    LIVINGROOM_KEYPOINTS2 = [
+        (276, 719),
+        (272, 558),
+        (271, 492),
+        (443, 521),
+        (443, 592),
+        (444, 679),
+        (703, 569),
+        (702, 644),
+        (140, 705),
+        (189, 746),
+        (237, 614),
+        (314, 463),
+        (661, 510),
+        (87, 407),
+    ]
+
+    # display_img_with_keypoints(im1, LIVINGROOM_KEYPOINTS1)
+    # display_img_with_keypoints(im2, LIVINGROOM_KEYPOINTS2)
+    mosaic = build_mosaic(im1, im2, LIVINGROOM_KEYPOINTS1, LIVINGROOM_KEYPOINTS2)
+    skio.imsave(f"../images/livingroom_mosaic.jpg", mosaic)
+
+    im1 = skio.imread(f"../data/shoes1.JPG")
+    im2 = skio.imread(f"../data/shoes2.JPG")
+
+    SHOES_KEYPOINTS1 = [
+        (184, 428),
+        (557, 429),
+        (201, 612),
+        (540, 612),
+        (213, 764),
+        (528, 766),
+        (225, 894),
+        (516, 896),
+    ]
+    SHOES_KEYPOINTS2 = [
+        (179, 242),
+        (570, 239),
+        (210, 434),
+        (542, 431),
+        (233, 571),
+        (522, 571),
+        (249, 681),
+        (508, 681),
+    ]
+
+    # display_img_with_keypoints(im1, SHOES_KEYPOINTS1)
+    # display_img_with_keypoints(im2, SHOES_KEYPOINTS2)
+    mosaic = build_mosaic(im1, im2, SHOES_KEYPOINTS1, SHOES_KEYPOINTS2)
+    skio.imsave(f"../images/shoes_mosaic.jpg", mosaic)
+
+
+# Main
+image_rectification()
+mosaic_blending()
--- code/utils.py
+++ code/utils.py
@@ -1,12 +1,18 @@
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
+import matplotlib.pyplot as plt
+import skimage.io as skio
+import cv2
+
 
 # ===== Helpers ======
+
 
 # Appends a one to each coordinate
 def homogeneous_coords(coords):
     ones = np.ones((coords.shape[0], 1))
     return np.hstack([coords, ones])
+
 
 # Samples the color for each coordinate using bilinear interpolation. Coords outside the image default to 0.
 def bilinear_interpolation(img, coords):
@@ -16,20 +22,32 @@ def bilinear_interpolation(img, coords):
     x = np.arange(width)
     y = np.arange(height)
 
-    if len(img.shape) == 2: # Grayscale image
-        interpolator = RegularGridInterpolator((y, x), img, bounds_error=False, fill_value=0)
+    if len(img.shape) == 2:  # Grayscale image
+        interpolator = RegularGridInterpolator(
+            (y, x), img, bounds_error=False, fill_value=0
+        )
         interpolated_colors = interpolator(coords)
-    else: # RGB image
+    else:  # RGB image
         interpolated_colors = []
         for c in range(3):
-            interpolator = RegularGridInterpolator((y, x), img[:, :, c], bounds_error=False, fill_value=0)
+            interpolator = RegularGridInterpolator(
+                (y, x), img[:, :, c], bounds_error=False, fill_value=0
+            )
             interpolated_colors.append(interpolator(coords))
         interpolated_colors = np.stack(interpolated_colors, axis=-1)
 
     return interpolated_colors
 
 
+def display_img_with_keypoints(img, keypoints):
+    plt.imshow(img)
+    x_coords, y_coords = zip(*keypoints)
+    plt.scatter(x_coords, y_coords, color="red", marker="o", s=10)
+    plt.show()
+
+
 # ===== Algorithms =====
+
 
 # Returns the homography matrix that maps im1 to im2 using given correspondances
 def compute_homography(im1_pts, im2_pts):
@@ -38,8 +56,8 @@ def compute_homography(im1_pts, im2_pts):
     y = []
 
     for i in range(n):
-        y1, x1 = im1_pts[i]
-        y2, x2 = im2_pts[i]
+        x1, y1 = im1_pts[i]
+        x2, y2 = im2_pts[i]
         A.append([x1, y1, 1, 0, 0, 0, -x1 * x2, -y1 * x2])
         A.append([0, 0, 0, x1, y1, 1, -x1 * y2, -y1 * y2])
         y.extend([x2, y2])
@@ -54,12 +72,15 @@ def compute_homography(im1_pts, im2_pts):
     H = np.append(h, 1).reshape(3, 3)
     return H
 
-# Returns the warped image after applying the given homography transformation
+
+# Returns the warped image after applying a homography and a shift vector to bring it to its "true" coordinates
 def warp_image(im, H):
     h, w = im.shape[:2]
-    corners = np.array([[0, 0, 1], [w-1, 0, 1], [w-1, h-1, 1], [0, h-1, 1]])
-    corners = (H @ np.reshape(corners, (4, 3, 1))) # 4 column vectors
-    corners = np.array([corner / corner[2] for corner in corners]) # account for scaling factor w
+    corners = np.array([[0, 0, 1], [w - 1, 0, 1], [w - 1, h - 1, 1], [0, h - 1, 1]])
+    corners = H @ np.reshape(corners, (4, 3, 1))  # 4 column vectors
+    corners = np.array(
+        [corner / corner[2] for corner in corners]
+    )  # account for scaling factor w
 
     corners = np.squeeze(corners)
     x_values = corners[:, 0]
@@ -79,13 +100,56 @@ def warp_image(im, H):
     xs = xs / ws
     ys = ys / ws
 
-    colors = bilinear_interpolation(im, np.column_stack((xs, ys)))
+    colors = bilinear_interpolation(im, np.column_stack((ys, xs)))
     colors = np.reshape(colors, (max_y - min_y, max_x - min_x, 3))
 
-    return colors.astype(np.uint8)
+    return colors.astype(np.uint8), min_x, min_y
 
 
 # Return a warped version of the image with a "straightened out" rectangle
 def rectify_image(im, im_pts, rect_pts):
     H = compute_homography(np.array(im_pts), np.array(rect_pts))
-    return warp_image(im, H)
+    ret, _, _ = warp_image(im, H)
+    return ret
+
+
+# Returns a mosaic of two images, where the second image is projected onto the first
+def build_mosaic(im1, im2, im1_pts, im2_pts):
+    H = compute_homography(np.array(im2_pts), np.array(im1_pts))
+    warped_im2, dx, dy = warp_image(im2, H)
+
+    im1_min_x, im1_max_x = 0, im1.shape[1] - 1
+    im1_min_y, im1_max_y = 0, im1.shape[0] - 1
+    warped_im2_min_x, warped_im2_max_x = dx, dx + warped_im2.shape[1] - 1
+    warped_im2_min_y, warped_im2_max_y = dy, dy + warped_im2.shape[0] - 1
+
+    min_x = min(im1_min_x, warped_im2_min_x)
+    max_x = max(im1_max_x, warped_im2_max_x)
+    min_y = min(im1_min_y, warped_im2_min_y)
+    max_y = max(im1_max_y, warped_im2_max_y)
+
+    ret = np.zeros((max_y - min_y + 1, max_x - min_x + 1, 3))
+    shift_x, shift_y = -min_x, -min_y
+
+    # Shift and place im1 into ret
+    ret[shift_y : shift_y + im1.shape[0], shift_x : shift_x + im1.shape[1]] = im1
+
+    # Shift and place warped_im2 into the correct position in ret
+    warped_y_start = warped_im2_min_y + shift_y
+    warped_y_end = warped_y_start + warped_im2.shape[0]
+    warped_x_start = warped_im2_min_x + shift_x
+    warped_x_end = warped_x_start + warped_im2.shape[1]
+
+    # Extract the region where warped_im2 will be placed
+    ret_region = ret[warped_y_start:warped_y_end, warped_x_start:warped_x_end]
+
+    # Find non-zero pixels in ret_region (overlapping pixels)
+    overlap_mask = ret_region != 0
+
+    # Average overlapping pixels
+    ret_region[overlap_mask] = (ret_region[overlap_mask] + warped_im2[overlap_mask]) / 2
+
+    # Assign non-overlapping pixels directly
+    ret_region[~overlap_mask] = warped_im2[~overlap_mask]
+
+    return ret.astype(np.uint8)
